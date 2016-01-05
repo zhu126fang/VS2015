a79f5807083be3d97e3b0aeaebf1ea5635b692a4
zhu126fang <zhu126fang@126.com>
2016-01-05 13:11:39 +0800

    Hello

--- Hello/Form1.Designer.vb
+++ Hello/Form1.Designer.vb
@@ -31,7 +31,7 @@ Partial Class Form1
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(100, 40)
         Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "Hello"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Form1
