62db13927a666bb0bfa5b5ebcf6d0e440050f400
Francis.jun <zhu126fang@126.com>
2016-03-31 16:06:27 +0800

    C# 打开Excel

---
+++ Excel_Open/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Microsoft.Office.Core;
+using Microsoft.Office.Interop.Excel;
+using System.IO;
+using System.Reflection;
+
+namespace Excel_Open
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+            Workbooks wbks = app.Workbooks;
+            try
+            {
+                _Workbook _wbk = wbks.Open(@"D:\Users\Administrator\Documents\Visual Studio 2015\Projects\Excel_Demo\Demo.xls");
+                app.Visible = true;
+            }
+            catch
+            {
+                MessageBox.Show("打开文件失败");
+            }
+        }
+    }
+}
