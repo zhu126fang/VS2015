da70a877b8dd76aa413000654b280fc9b555f918
Francis.jun <zhu126fang@126.com>
2016-03-31 17:00:23 +0800

    测试代码的推送功能

--- Hello/Form1.vb
+++ Hello/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        //测试VS2015的调试环境
+        //测试VS2015的调试环境，测试代码修改推送功能
         MsgBox("Hello  World!")
     End Sub
 End Class
