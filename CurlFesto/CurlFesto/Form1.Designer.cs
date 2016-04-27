845a07848a8bfc028ca0f0f355271845c1eb4a9e
Francis.jun <zhu126fang@126.com>
2016-04-27 12:21:59 +0800

    添加完善功能

--- CurlFesto/CurlFesto/Form1.Designer.cs
+++ CurlFesto/CurlFesto/Form1.Designer.cs
@@ -28,24 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(38, 42);
-            this.textBox1.Multiline = true;
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(38, 25);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(149, 46);
+            this.textBox1.Size = new System.Drawing.Size(428, 29);
             this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(38, 146);
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(363, 120);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(108, 60);
+            this.button1.Size = new System.Drawing.Size(103, 45);
             this.button1.TabIndex = 1;
             this.button1.Text = "High";
             this.button1.UseVisualStyleBackColor = true;
@@ -53,19 +65,130 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(250, 146);
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(363, 186);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(108, 60);
+            this.button2.Size = new System.Drawing.Size(103, 46);
             this.button2.TabIndex = 2;
             this.button2.Text = "Low";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(38, 412);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 46);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "OPEN";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button4.Location = new System.Drawing.Point(199, 412);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 46);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "CLOSE";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 21;
+            this.listBox1.Location = new System.Drawing.Point(38, 87);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(190, 319);
+            this.listBox1.TabIndex = 6;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button5.Location = new System.Drawing.Point(363, 412);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(103, 46);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Test";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 21;
+            this.listBox2.Location = new System.Drawing.Point(257, 87);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(87, 319);
+            this.listBox2.TabIndex = 8;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button6.Location = new System.Drawing.Point(363, 281);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(103, 45);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "High";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button7.Location = new System.Drawing.Point(363, 344);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(103, 46);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "Low";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(368, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 21);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "主机箱压力";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(368, 257);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 21);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "气源箱压力";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(504, 463);
+            this.ClientSize = new System.Drawing.Size(492, 466);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
@@ -81,6 +204,16 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- CurlFesto/CurlFesto/Form1.cs
+++ CurlFesto/CurlFesto/Form1.cs
@@ -10,11 +10,47 @@ using System.Windows.Forms;
 using SeasideResearch.LibCurlNet;
 namespace CurlFesto
 {
-    public partial class Form1 : Form
+        public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+
+            try
+            {
+                Curl.GlobalInit((int)CURLinitFlag.CURL_GLOBAL_ALL);
+
+                Easy easy = new Easy();
+                Easy.WriteFunction wf = new Easy.WriteFunction(OnWriteData);
+
+                /////获取实时数据
+                easy.SetOpt(CURLoption.CURLOPT_URL, "http://192.168.2.50/variables/config");
+
+                //调用函数获取数据
+                easy.SetOpt(CURLoption.CURLOPT_WRITEFUNCTION, wf);
+                easy.Perform();
+                easy.Cleanup();
+
+                Curl.GlobalCleanup();
+            }
+            catch
+            {
+                MessageBox.Show("获取失败！");
+            }
+
+            string[] sArray = Global.msgtemp.Split(';');
+
+            listBox1.Items.Clear();
+            foreach (string i in sArray)
+            {
+                if (i != "") listBox1.Items.Add(i);
+            }
+    }
+
+        public static Int32 OnWriteData(Byte[] buf, Int32 size, Int32 nmemb, Object extraData)
+        {
+            Global.msgtemp = (System.Text.Encoding.UTF8.GetString(buf));
+            return size * nmemb;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -26,43 +62,21 @@ namespace CurlFesto
                 Easy easy = new Easy();
                 Easy.WriteFunction wf = new Easy.WriteFunction(OnWriteData);
 
-                /////开启数据更新
-                //easy.SetOpt(CURLoption.CURLOPT_URL, "http://192.168.2.50/variables/write.php");
-                //easy.SetOpt(CURLoption.CURLOPT_POSTFIELDS, "cmd=1");
-
-                /////关闭数据更新
-                //easy.SetOpt(CURLoption.CURLOPT_URL, "http://192.168.2.50/variables/write.php");
-                //easy.SetOpt(CURLoption.CURLOPT_POSTFIELDS, "cmd=0");
-
-                /////获取实时数据
-                //easy.SetOpt(CURLoption.CURLOPT_URL, "http://192.168.2.50/variables/mnt/symbols");
-
                 //设定主机箱压力为500Kpa
                 easy.SetOpt(CURLoption.CURLOPT_URL, "http://192.168.2.50/variables/write.php");
-                easy.SetOpt(CURLoption.CURLOPT_POSTFIELDS, "set=.Main_Pression_Set%3AINT%3D5000%3B&cmd=3");
+                easy.SetOpt(CURLoption.CURLOPT_POSTFIELDS, "set=.Main_Pression_Set%3AINT%3D5900%3B&cmd=3");
 
-                //设定主机箱压力为0Kpa
-                //easy.SetOpt(CURLoption.CURLOPT_URL, "http://192.168.2.50/variables/write.php");
-                //easy.SetOpt(CURLoption.CURLOPT_POSTFIELDS, "set=.Main_Pression_Set%3AINT%3D0%3B&cmd=3");
-
+                //调用函数获取数据
                 easy.SetOpt(CURLoption.CURLOPT_WRITEFUNCTION, wf);
                 easy.Perform();
                 easy.Cleanup();
 
                 Curl.GlobalCleanup();
-
             }
-            catch (Exception ex)
+            catch
             {
-                //Console.WriteLine(ex);
+                MessageBox.Show("错误");
             }
-            //Console.ReadKey();
-        }
-
-        public static Int32 OnWriteData(Byte[] buf, Int32 size, Int32 nmemb, Object extraData)
-        {
-            //Form1.textBox1.Text(System.Text.Encoding.UTF8.GetString(buf));
-            return size * nmemb;
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -73,10 +87,6 @@ namespace CurlFesto
 
                 Easy easy = new Easy();
                 Easy.WriteFunction wf = new Easy.WriteFunction(OnWriteData);
-
-                /////开启数据更新
-                //easy.SetOpt(CURLoption.CURLOPT_URL, "http://192.168.2.50/variables/write.php");
-                //easy.SetOpt(CURLoption.CURLOPT_POSTFIELDS, "cmd=1");
 
                 /////关闭数据更新
                 //easy.SetOpt(CURLoption.CURLOPT_URL, "http://192.168.2.50/variables/write.php");
@@ -100,9 +110,190 @@ namespace CurlFesto
                 Curl.GlobalCleanup();
 
             }
-            catch (Exception ex)
+            catch
             {
-                //Console.WriteLine(ex);
+                MessageBox.Show("错误2");
+            }
+        }
+
+        public  class Global
+        {
+            public static string msgtemp;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                Curl.GlobalInit((int)CURLinitFlag.CURL_GLOBAL_ALL);
+
+                Easy easy = new Easy();
+                Easy.WriteFunction wf = new Easy.WriteFunction(OnWriteData);
+
+                /////获取实时数据
+                easy.SetOpt(CURLoption.CURLOPT_URL, "http://192.168.2.50/variables/mnt/symbols");
+
+                //调用函数获取数据
+                easy.SetOpt(CURLoption.CURLOPT_WRITEFUNCTION, wf);
+                easy.Perform();
+                easy.Cleanup();
+
+                Curl.GlobalCleanup();
+            }
+            catch
+            {
+                textBox1.Text = "获取失败！";
+            }
+
+            textBox1.Text = Global.msgtemp;
+            string[] sArray = Global.msgtemp.Split(';');
+
+            listBox2.Items.Clear();
+            foreach (string i in sArray)
+            {
+                if (i !="")     listBox2.Items.Add(i);
+            }
+}
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Curl.GlobalInit((int)CURLinitFlag.CURL_GLOBAL_ALL);
+
+                Easy easy = new Easy();
+                Easy.WriteFunction wf = new Easy.WriteFunction(OnWriteData);
+
+                /////开启数据更新
+                easy.SetOpt(CURLoption.CURLOPT_URL, "http://192.168.2.50/variables/write.php");
+                easy.SetOpt(CURLoption.CURLOPT_POSTFIELDS, "cmd=1");
+
+                //调用函数获取数据
+                easy.SetOpt(CURLoption.CURLOPT_WRITEFUNCTION, wf);
+                easy.Perform();
+                easy.Cleanup();
+
+                Curl.GlobalCleanup();
+            }
+            catch
+            {
+                MessageBox.Show("打开失败！");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Curl.GlobalInit((int)CURLinitFlag.CURL_GLOBAL_ALL);
+
+                Easy easy = new Easy();
+                Easy.WriteFunction wf = new Easy.WriteFunction(OnWriteData);
+
+                /////关闭数据更新
+                easy.SetOpt(CURLoption.CURLOPT_URL, "http://192.168.2.50/variables/write.php");
+                easy.SetOpt(CURLoption.CURLOPT_POSTFIELDS, "cmd=0");
+
+                //调用函数获取数据
+                easy.SetOpt(CURLoption.CURLOPT_WRITEFUNCTION, wf);
+                easy.Perform();
+                easy.Cleanup();
+
+                Curl.GlobalCleanup();
+            }
+            catch
+            {
+                MessageBox.Show("关闭失败！");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Curl.GlobalInit((int)CURLinitFlag.CURL_GLOBAL_ALL);
+
+                Easy easy = new Easy();
+                Easy.WriteFunction wf = new Easy.WriteFunction(OnWriteData);
+
+                /////获取实时数据
+                easy.SetOpt(CURLoption.CURLOPT_URL, "http://192.168.2.50/variables/config");
+
+                //调用函数获取数据
+                easy.SetOpt(CURLoption.CURLOPT_WRITEFUNCTION, wf);
+                easy.Perform();
+                easy.Cleanup();
+
+                Curl.GlobalCleanup();
+            }
+            catch
+            {
+                textBox1.Text = "获取失败！";
+            }
+            timer1.Enabled = false;
+            textBox1.Text = Global.msgtemp;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Curl.GlobalInit((int)CURLinitFlag.CURL_GLOBAL_ALL);
+
+                Easy easy = new Easy();
+                Easy.WriteFunction wf = new Easy.WriteFunction(OnWriteData);
+
+                //设定主机箱压力为500Kpa
+                easy.SetOpt(CURLoption.CURLOPT_URL, "http://192.168.2.50/variables/write.php");
+                easy.SetOpt(CURLoption.CURLOPT_POSTFIELDS, "set=.Gas_Pression_Set%3AINT%3D5900%3B&cmd=3");
+
+                //调用函数获取数据
+                easy.SetOpt(CURLoption.CURLOPT_WRITEFUNCTION, wf);
+                easy.Perform();
+                easy.Cleanup();
+
+                Curl.GlobalCleanup();
+            }
+            catch
+            {
+                MessageBox.Show("错误");
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Curl.GlobalInit((int)CURLinitFlag.CURL_GLOBAL_ALL);
+
+                Easy easy = new Easy();
+                Easy.WriteFunction wf = new Easy.WriteFunction(OnWriteData);
+
+                /////关闭数据更新
+                //easy.SetOpt(CURLoption.CURLOPT_URL, "http://192.168.2.50/variables/write.php");
+                //easy.SetOpt(CURLoption.CURLOPT_POSTFIELDS, "cmd=0");
+
+                /////获取实时数据
+                //easy.SetOpt(CURLoption.CURLOPT_URL, "http://192.168.2.50/variables/mnt/symbols");
+
+                //设定主机箱压力为500Kpa
+                //easy.SetOpt(CURLoption.CURLOPT_URL, "http://192.168.2.50/variables/write.php");
+                //easy.SetOpt(CURLoption.CURLOPT_POSTFIELDS, "set=.Main_Pression_Set%3AINT%3D5000%3B&cmd=3");
+
+                //设定主机箱压力为0Kpa
+                easy.SetOpt(CURLoption.CURLOPT_URL, "http://192.168.2.50/variables/write.php");
+                easy.SetOpt(CURLoption.CURLOPT_POSTFIELDS, "set=.Gas_Pression_Set%3AINT%3D0%3B&cmd=3");
+
+                easy.SetOpt(CURLoption.CURLOPT_WRITEFUNCTION, wf);
+                easy.Perform();
+                easy.Cleanup();
+
+                Curl.GlobalCleanup();
+
+            }
+            catch
+            {
+                MessageBox.Show("错误2");
             }
         }
     }
