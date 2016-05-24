d09db7a9e482d2af3a82e5e461e9c4405efbbd5d
Francis.jun <zhu126fang@126.com>
2016-05-24 12:04:36 +0800

    完善功能

--- Screenshot/Screenshot/Form1.Designer.cs
+++ Screenshot/Screenshot/Form1.Designer.cs
@@ -29,11 +29,48 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(73, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "截图中...";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 119);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Screen";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Screenshot/Screenshot/Form1.cs
+++ Screenshot/Screenshot/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
 
 namespace Screenshot
 {
@@ -15,6 +18,43 @@ namespace Screenshot
         public Form1()
         {
             InitializeComponent();
+    }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 500;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            int picwidth = 1366;
+            int picheight = 768;
+            //picwidth = Screen.PrimaryScreen.Bounds.Width;
+            //picheight = Screen.PrimaryScreen.Bounds.Height;
+
+            //日期和目录
+            System.DateTime currentTime = new System.DateTime();
+            currentTime = System.DateTime.Now;
+            string strYMD = currentTime.ToString("yyyyMMdd");
+            string strHMS = currentTime.ToString("HHmmss");
+            string path1 = @"D:\Report";
+            string path2 = @"D:\Report\" + strYMD;
+            if (!Directory.Exists(path1)) Directory.CreateDirectory(path1);
+            if (!Directory.Exists(path2)) Directory.CreateDirectory(path2);
+            string filename = @"D:\Report\" + strYMD + @"\" + strYMD + "_" + strHMS + ".jpg";
+
+            //Thread.Sleep(10000);
+
+            //创建图象，保存将来截取的图象
+            Bitmap image = new Bitmap(picwidth, picheight);
+            Graphics imgGraphics = Graphics.FromImage(image);
+            //设置截屏区域 可定义
+            imgGraphics.CopyFromScreen(0, 0, 0, 0, new Size(picwidth, picheight));
+
+            //保存图片
+            //MessageBox.Show (filename);
+            image.Save(filename);
+            System.Environment.Exit(0);
         }
     }
 }
