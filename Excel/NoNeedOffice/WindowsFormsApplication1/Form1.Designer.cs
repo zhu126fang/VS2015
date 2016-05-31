ab5228244589ca422614a1c51a9e577dc99ce815
Francis.jun <zhu126fang@126.com>
2016-05-31 17:05:45 +0800

    修改按钮

--- Excel/NoNeedOffice/WindowsFormsApplication1/Form1.Designer.cs
+++ Excel/NoNeedOffice/WindowsFormsApplication1/Form1.Designer.cs
@@ -34,21 +34,23 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(106, 131);
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(96, 89);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 40);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Write";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(123, 191);
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(96, 150);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(100, 40);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Read";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -56,7 +58,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.ClientSize = new System.Drawing.Size(292, 239);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
--- Excel/NoNeedOffice/WindowsFormsApplication1/Form1.cs
+++ Excel/NoNeedOffice/WindowsFormsApplication1/Form1.cs
@@ -42,19 +42,9 @@ namespace WindowsFormsApplication1
                     for (int m = i * inv; m < mCount && m < (i + 1) * inv; m++)
                     {
                         f++;
-                        cells.Add(f, 1,"<npc=3571>：“韩赵之争自新朝以来愈演愈烈，如今终于得以收场，实在是件幸事。为防韩侂胄倒戈相向，我和你秋姨斟酌再三之后决定为你寻一同伴，行走江湖也好有个照应，只是这人选还需要你自行去寻访。”<end>"+
-"<playername>：“怎么个寻访法？”<end>"+
-"<npc=3571>：“目前在<color=yellow>野外地图<color>和<color=yellow>逍遥谷<color>内寻访即可，以后会陆续开放更多。你要留意<color=yellow>人物选择界面<color>名字右边是否有一个<color=yellow>“心形标记”<color>，如果有，表示他可以被说服。不过，说服的规矩也各有不同。”<end>"+
-"<playername>：“有什么不同？”<end>"+
-"<npc=3571>：“说服之前需要先备<color=yellow>“名帖”<color>，名帖上写着姓字名谁，师承何处，哪里人士，所循何道等等。有如人之衣裳，名帖依贵贱分为<color=yellow>镶边帛帖、穿珠银帖、鎏金玉帖<color>三种，用以说服不同人士。<color=yellow>镶边帛帖<color>可用来说服15-55级野外地图的人形怪，<color=yellow>穿珠银帖<color>可用来说服逍遥谷的人形怪，至于<color=yellow>鎏金玉帖<color>目前尚未开放。”<end>"+
-"<playername>：“原来如此，那这<color=yellow>“名帖”<color>又是怎么用的？”<end>"+
-"<npc=3571>：“首先找到可被说服的人形怪，在他附近<color=yellow>“选中他”<color>，切忌不要动手打他。选中之后<color=yellow>“鼠标右键单击名帖”<color>，便会有一个<color=yellow>“进度条提示”<color>你正在说服。此时不要做任何操作，耐心等着就是。一旦说服成功，会有提示告知你。若是不成功，你的名帖就会消失。”<end>"+
-"<playername>：“这样就可以了吗？”<end>"+
-"<npc=3571>：“是的！你按照我所说的去说服一名同伴带回来吧！不过你需要答对我提的问题，我才可以将名帖交给你。”<end>");
-                       
+                        cells.Add(f, 1,"测试");                       
                     }
                 }
-
                 doc.Save(ofd.SelectedPath,true);
             }
         }
