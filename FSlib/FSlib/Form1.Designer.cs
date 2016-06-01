31c393f31db9dc2a5175b3feab1a6c0c8e395791
Francis.jun <zhu126fang@126.com>
2016-06-01 16:08:10 +0800

    FSlib库文件测试

--- FSlib/FSlib/Form1.Designer.cs
+++ FSlib/FSlib/Form1.Designer.cs
@@ -28,12 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnGrabBdPage = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnGrabBdLogo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGrabBdPage
+            // 
+            this.btnGrabBdPage.Location = new System.Drawing.Point(45, 15);
+            this.btnGrabBdPage.Name = "btnGrabBdPage";
+            this.btnGrabBdPage.Size = new System.Drawing.Size(100, 40);
+            this.btnGrabBdPage.TabIndex = 0;
+            this.btnGrabBdPage.Text = "抓取百度首页";
+            this.btnGrabBdPage.UseVisualStyleBackColor = true;
+            this.btnGrabBdPage.Click += new System.EventHandler(this.btnGrabBdPage_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(28, 106);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResult.Size = new System.Drawing.Size(562, 294);
+            this.txtResult.TabIndex = 1;
+            // 
+            // btnGrabBdLogo
+            // 
+            this.btnGrabBdLogo.Location = new System.Drawing.Point(170, 15);
+            this.btnGrabBdLogo.Name = "btnGrabBdLogo";
+            this.btnGrabBdLogo.Size = new System.Drawing.Size(100, 40);
+            this.btnGrabBdLogo.TabIndex = 2;
+            this.btnGrabBdLogo.Text = "抓取百度LOGO";
+            this.btnGrabBdLogo.UseVisualStyleBackColor = true;
+            this.btnGrabBdLogo.Click += new System.EventHandler(this.btnGrabBdLogo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 432);
+            this.Controls.Add(this.btnGrabBdLogo);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnGrabBdPage);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGrabBdPage;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnGrabBdLogo;
     }
 }
 
--- FSlib/FSlib/Form1.cs
+++ FSlib/FSlib/Form1.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using FSLib.Network.Http;
+using FSLib.Network;       //需要引用net45的dll文件
+
 namespace FSlib
 {
     public partial class Form1 : Form
@@ -15,6 +18,79 @@ namespace FSlib
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void btnGrabBdPage_Click(object sender, EventArgs e)
+        {
+            InitWorkingUI();
+            AppendText("正在请求 https://www.baidu.com/ ...");
+
+            var client = new HttpClient();
+            var context = client.Create<string>(HttpMethod.Get, "https://www.baidu.com/");     ///需要修改为https
+            await context.SendTask();
+
+            if (context.IsValid())
+            {
+                AppendText(context.Result);
+            }
+            else
+            {
+                AppendText("错误！状态码：" + context.Response.Status);
+                AppendText("服务器响应：" + context.ResponseContent);
+            }
+            WorkingFinished();
+        }
+
+        #region 辅助函数
+        void InitWorkingUI()
+        {
+            txtResult.Clear();
+            //pgWaiting.Visible = true;
+        }
+
+        void AppendText(string txt)
+        {
+            txtResult.AppendText(txt);
+            txtResult.AppendText(Environment.NewLine);
+            txtResult.AppendText(Environment.NewLine);
+            txtResult.ScrollToCaret();
+        }
+
+        void WorkingFinished()
+        {
+            //pgWaiting.Visible = false;
+        }
+        #endregion
+
+        private async void btnGrabBdLogo_Click(object sender, EventArgs e)
+        {
+            InitWorkingUI();
+
+
+            AppendText("正在请求 http://www.baidu.com/img/bdlogo.png ...");
+
+            var client = new HttpClient();
+            var context = client.Create<Image>(HttpMethod.Get, "http://www.baidu.com/img/bdlogo.png");
+            await context.SendTask();
+
+            if (context.IsValid())
+            {
+                AppendText("正在显示图片...");
+
+                var form = new Form() { Size = context.Result.Size };
+                var pic = new PictureBox();
+                form.Controls.Add(pic);
+                pic.Dock = DockStyle.Fill;
+                pic.Image = context.Result;
+                form.Show();
+                form.Activate();
+            }
+            else
+            {
+                AppendText("错误！状态码：" + context.Response.Status);
+                AppendText("服务器响应：" + context.ResponseContent);
+            }
+            WorkingFinished();
         }
     }
 }
