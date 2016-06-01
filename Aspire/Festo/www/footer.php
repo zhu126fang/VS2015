4d3e4dd93edf253ca9ea711a8c3c6e042a3f567e
Francis.jun <zhu126fang@126.com>
2016-06-01 12:22:06 +0800

    删除Aspire

--- Aspire/Festo/www/footer.php
+++
@@ -1,3 +0,0 @@
-<div id="footer">
-	<p align="center">&copy;2005-2009 Festo AG & Co. KG | <a href="#" onclick="loadModule('settings');">page settings</a> | <a href="help.htm">help</a></p>
-</div> <!-- footer -->
