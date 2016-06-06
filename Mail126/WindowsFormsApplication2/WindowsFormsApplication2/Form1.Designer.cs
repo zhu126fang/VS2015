2349b25cf6d1dc2f19edd42a4e73103c522491ed
Francis.jun <zhu126fang@126.com>
2016-06-06 13:19:27 +0800

    添加登录126邮箱文件

---
+++ Mail126/WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblFlavor = new System.Windows.Forms.Label();
+            this.cmdSubmit = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 28);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(41, 12);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "帐号：";
+            // 
+            // lblFlavor
+            // 
+            this.lblFlavor.AutoSize = true;
+            this.lblFlavor.Location = new System.Drawing.Point(12, 67);
+            this.lblFlavor.Name = "lblFlavor";
+            this.lblFlavor.Size = new System.Drawing.Size(41, 12);
+            this.lblFlavor.TabIndex = 1;
+            this.lblFlavor.Text = "密码：";
+            // 
+            // cmdSubmit
+            // 
+            this.cmdSubmit.Location = new System.Drawing.Point(96, 100);
+            this.cmdSubmit.Name = "cmdSubmit";
+            this.cmdSubmit.Size = new System.Drawing.Size(94, 38);
+            this.cmdSubmit.TabIndex = 2;
+            this.cmdSubmit.Text = "登录";
+            this.cmdSubmit.UseVisualStyleBackColor = true;
+            this.cmdSubmit.Click += new System.EventHandler(this.cmdSubmit_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(59, 64);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(196, 21);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(59, 25);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(196, 21);
+            this.txtUsername.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 159);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.cmdSubmit);
+            this.Controls.Add(this.lblFlavor);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.Text = "126邮箱一键登录";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblFlavor;
+        private System.Windows.Forms.Button cmdSubmit;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+    }
+}
+
---
+++ Mail126/WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdSubmit_Click(object sender, EventArgs e)
+        {
+            SHDocVw.InternetExplorer ie;
+            object vPost, vHeaders, vFlags, vTargetFrame, vUrl;
+            string username, password, cPostData;
+            vFlags = null;
+            vTargetFrame = null;
+            vUrl = " https://mail.126.com/entry/cgi/ntesdoor";
+            username = txtUsername.Text;
+            password = txtPassword.Text;
+            cPostData = "funcid=loginone&passtype=1&product=mail126&username="+username+"&password="+password;
+            vHeaders = "Content-Type: application/x-www-form-urlencoded" + Convert.ToChar(10) + Convert.ToChar(13);
+            
+            //Convert the string to post to an array of bytes.
+            vPost = ASCIIEncoding.ASCII.GetBytes(cPostData);
+            //Create an instance of Internet Explorer and make it visible.
+            ie = new SHDocVw.InternetExplorer();
+            ie.Visible = true;
+            ie.Navigate2(ref vUrl, ref vFlags, ref vTargetFrame, ref vPost, ref vHeaders);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ///以默认密码方式显示密码字符
+            txtPassword.UseSystemPasswordChar = true;
+        }
+    }
+}
