cfa6d54cda2c0a5f40bf6b69a3ea0752b980d703
Francis.jun <zhu126fang@126.com>
2016-06-14 14:10:53 +0800

    修改为窗口显示模式

---
+++ ManagedWifi/WifiExample/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WifiExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxlog = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBoxlog
+            // 
+            this.listBoxlog.FormattingEnabled = true;
+            this.listBoxlog.ItemHeight = 12;
+            this.listBoxlog.Location = new System.Drawing.Point(12, 21);
+            this.listBoxlog.Name = "listBoxlog";
+            this.listBoxlog.Size = new System.Drawing.Size(246, 340);
+            this.listBoxlog.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 385);
+            this.Controls.Add(this.listBoxlog);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxlog;
+    }
+}
